44ee0a94be5fceaaa95c894e16400e415e72f09b
Jeffrey-Kai Li <57923095+jeffreykaili@users.noreply.github.com>
2021-09-19 08:42:16 -0400

    aaaaa

---
+++ lib/pages/market_place/marketplace_tile.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MarketplaceTile extends StatelessWidget {
+  final imgURL;
+  final company;
+  final price;
+  MarketplaceTile({
+    @required this.imgURL,
+    @required this.company,
+    @required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        child: Stack(
+          children: [
+            Card(
+              elevation: 4.5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Container(
+                height: 180,
+                width: MediaQuery.of(context).size.width,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: FittedBox(
+                    child: Opacity(
+                      opacity: 0.75,
+                      child: Image.network(imgURL),
+                    ),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: 80,
+                  width: MediaQuery.of(context).size.width - 26,
+                  child: Padding(
+                      padding: EdgeInsets.only(
+                        left: 10,
+                        right: 10,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15.0),
+                                ),
+                              ),
+                              primary: Color(0xFF4E47C6),
+                            ),
+                            child: Text(
+                              "Redeem for " + price + " EXE",
+                              style: GoogleFonts.poppins(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 20,
+                                height: 1.2,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(15.0),
+                      bottomRight: Radius.circular(15.0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
